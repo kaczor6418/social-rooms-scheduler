cf762c58b79ec096e04fbafefdcea9dc08acb3d3
kaczor6418 <k.kaczynski97@gmail.com>
2021-04-19 21:40:10 +0200

    chore(project): add and configure linters

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,11 +5,13 @@ import { store } from './app/store';
 import App from './App';
 
 test('renders learn react link', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   expect(getByText(/learn/i)).toBeInTheDocument();
 });
